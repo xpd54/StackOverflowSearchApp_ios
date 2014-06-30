00830a60b6437ec5c906e1611aea1beb840dfe47
Ravi Prakash <ravi@helpshift.com>
2014-06-30 10:47:02 +0530

    Alert view on dispatch main queue .

--- Stacky/Stacky/Alert.m
+++ Stacky/Stacky/Alert.m
@@ -10,25 +10,52 @@
 
 @implementation Alert
 -(void) showAlertsForInterConnection {
-    UIAlertView *internetConnectionAlert = [[UIAlertView alloc] initWithTitle:@"Internet Connection" message:@"You don't have Internet Connection \n Try Again" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-    [internetConnectionAlert show];
+//    UIAlertView *internetConnectionAlert = [[UIAlertView alloc] initWithTitle:@"Internet Connection" message:@"You don't have Internet Connection \n Try Again" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+//    [internetConnectionAlert show];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertView *internetConnectionAlert = [[UIAlertView alloc] initWithTitle:@"Internet Connection" message:@"You don't have Internet Connection \n Try Again" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [internetConnectionAlert show];
+    });
+    
 }
 
 -(void) showDatabaseMissingWithSearchText:(NSString *)searchText {
-    NSString *popupMessage = [[NSString alloc] initWithFormat:@"You Don't Have Any Offline Data For %@",searchText];
-    UIAlertView *databaseMissing = [[UIAlertView alloc] initWithTitle:@"Database Missing" message:popupMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-    [databaseMissing show];
+//    NSString *popupMessage = [[NSString alloc] initWithFormat:@"You Don't Have Any Offline Data For %@",searchText];
+//    UIAlertView *databaseMissing = [[UIAlertView alloc] initWithTitle:@"Database Missing" message:popupMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+//    [databaseMissing show];
+    
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *popupMessage = [[NSString alloc] initWithFormat:@"You Don't Have Any Offline Data For %@",searchText];
+        UIAlertView *databaseMissing = [[UIAlertView alloc] initWithTitle:@"Database Missing" message:popupMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [databaseMissing show];
+    });
+    
+    
 }
 
 -(void) showOfflineResultWithSearchText:(NSString *)searchText {
-    NSString *popupMessage = [[NSString alloc] initWithFormat:@"Showing Offline Result For %@",searchText];
-    UIAlertView *offlineResult = [[UIAlertView alloc] initWithTitle:@"Offline Data" message:popupMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-    [offlineResult show];
+//    NSString *popupMessage = [[NSString alloc] initWithFormat:@"Showing Offline Result For %@",searchText];
+//    UIAlertView *offlineResult = [[UIAlertView alloc] initWithTitle:@"Offline Data" message:popupMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+//    [offlineResult show];
+//    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *popupMessage = [[NSString alloc] initWithFormat:@"Showing Offline Result For %@",searchText];
+        UIAlertView *offlineResult = [[UIAlertView alloc] initWithTitle:@"Offline Data" message:popupMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [offlineResult show];
+    });
+    
 }
 
 -(void) showErrorForNoSearchData {
-    UIAlertView *error = [[UIAlertView alloc] initWithTitle:@"Error" message:@"No Search Data Found" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-    [error show];
+//    UIAlertView *error = [[UIAlertView alloc] initWithTitle:@"Error" message:@"No Search Data Found" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+//    [error show];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertView *error = [[UIAlertView alloc] initWithTitle:@"Error" message:@"No Search Data Found" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [error show];
+    });
 }
 
 @end
--- Stacky/Stacky/viewIndicatorViewController.m
+++ Stacky/Stacky/viewIndicatorViewController.m
@@ -38,6 +38,22 @@
 
 -(void) startAnimation : (UIViewController *) viewOfIndicator {
     [self performSelectorInBackground:@selector(activityIndicator:) withObject:viewOfIndicator];
+    
+//    dispatch_async(dispatch_get_main_queue(),^{
+//        
+//        UIView *transparetSubView = [[UIView alloc] initWithFrame:viewOfIndicator.view.bounds];
+//        transparetSubView.tag = 6;
+//        transparetSubView.backgroundColor = [UIColor blackColor];
+//        transparetSubView.alpha = 0.8;
+//        transparetSubView.opaque = NO;
+//        _activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(viewOfIndicator.view.center.x,viewOfIndicator.view.center.y, 0.0f, 0.0f)];
+//        [_activityIndicator setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleWhiteLarge];
+//        [_activityIndicator startAnimating];
+//        [transparetSubView addSubview:_activityIndicator];
+//        [viewOfIndicator.view addSubview:transparetSubView];
+//
+//    
+//    });
 }
 
 -(void) activityIndicator : (UIViewController *)viewOfIndicator {
